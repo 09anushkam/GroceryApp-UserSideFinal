0ada3c8df85fcc72b2f60410b0c2ae6be73d333c
kinshupanchal <kinshumpanchal@gmail.com>
2024-10-30 20:33:38 +0530

    Kinshu's Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/phone.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: 'home_screen1',
+    initialRoute: 'home_screen',
     debugShowCheckedModeBanner: false,
     routes: {
+  'phone': (context) => MyPhone(),
 
-      ),
+
     },
     theme: ThemeData(
       primarySwatch: Colors.green,
